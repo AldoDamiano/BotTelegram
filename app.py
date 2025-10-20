503adc189fb81fef7be092aafc6d871480c8a55f
AldoDamiano <161452544+AldoDamiano@users.noreply.github.com>
2025-10-20 21:47:34 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+import requests
+import os
+
+app = Flask(__name__)
+
+TOKEN = os.getenv("BOT_TOKEN")
+TELEGRAM_URL = f"https://api.telegram.org/bot{TOKEN}"
+
+@app.route('/')
+def home():
+    return "Bot Telegram attivo su Render!"
+
+@app.route(f"/{TOKEN}", methods=["POST"])
+def receive_update():
+    data = request.get_json()
+
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+        send_message(chat_id, f"Ciao 👋 hai scritto: {text}")
+
+    return {"ok": True}
+
+def send_message(chat_id, text):
+    url = f"{TELEGRAM_URL}/sendMessage"
+    payload = {"chat_id": chat_id, "text": text}
+    requests.post(url, json=payload)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
